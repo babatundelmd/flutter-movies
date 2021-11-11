6603e0ff4e600fe9af2440f8c19e0da0eb5e7aee
Babatunde Lamidi <odunayohlamidi@gmail.com>
2021-11-11 07:44:47 +0100

    clean up

--- nuxt.config.js
+++ nuxt.config.js
@@ -50,7 +50,7 @@ export default {
   // Build Configuration: https://go.nuxtjs.dev/config-build
   build: {},
 
-  privateRuntimeConfig: {
-    apiKey: process.env.PRIVATE_KEY || '',
+  env: {
+    apiKey: process.env.API_KEY || '',
   },
 }
